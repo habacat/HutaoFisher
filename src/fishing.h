ce23d69f0f22e78670c973b3796313be6818be3c
habacat <pb_hbc@163.com>
2023-08-22 13:40:54 +0800

    修改等待时间和重试次数2

--- src/fishing.h
+++ src/fishing.h
@@ -44,14 +44,17 @@ class Fisher {
       {1, 0},    {1, 0},    {1, 0},    {1, 0},    {1, 0},    {0, -1},
       {0.75, 0}, {0.75, 0}, {0.75, 0}, {0.75, 0}, {0.75, 0}, {0.75, 0},
       {0.75, 0}, {0.75, 0}, {0.75, 0}, {0.75, 0}};
-  const int MaxRodTries = 25;
-  const int MaxFishFailNum = 15;  // at most 5 times miss fish can be tolerated
+  const int MaxRodTries = 20;
+  const int MaxFishFailNum = 30;  // at most 5 times miss fish can be tolerated
   // unit: second
-  const double MaxThrowWaiting = 8;
+  const double MaxThrowWaiting = 20;
+  //   const double MaxBiteWaiting[FISH_CLASS_NUM] = {
+  //       8,    8.5,  9.5,  10.5, 8.5,
+  //       11.5, 11.5, 10.5, 9.5,  8.5};  // index is fish label
   const double MaxBiteWaiting[FISH_CLASS_NUM] = {
-      8,    8.5,  9.5,  10.5, 8.5,
-      11.5, 11.5, 10.5, 9.5,  8.5};  // index is fish label
-  const double MaxControlWaiting = 8;
+	  120, 120, 120, 120, 120,
+	  120, 120, 120, 120, 120}; // index is fish label
+  const double MaxControlWaiting = 20;
 
   bool typeToFish[FISH_CLASS_NUM];
   bool logAllImgs;
